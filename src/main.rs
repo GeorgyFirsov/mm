cb9889626488ba91591a26cb8d4a3e7fb0f8d681
GeorgyFirsov <gfirsov007@gmail.com>
2022-12-30 15:24:50 +0300

    docs: comments

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+//! # mm
+//! 
+//! `mm` is a simple command line program, that stores your notes on your computer. 
+//! Internally it uses `git` to track all the changes step-by-step and allow a user 
+//! to get back to any state.
+
+
 mod repo;
 
 extern crate clap;
 extern crate git2;
+
 
 fn main() {
     
